8a11c26665141d1e8bf7f29c980d718ad1bb69c8
Charles Altopiedi <Chuckalto@gmail.com>
2022-01-20 20:20:14 -0500

    was able to display server data to my front end

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React from 'react';
-import Card from 'react-bootstrap/Card';
-
+import { Card, ListGroup, Container, ListGroupItem } from 'react-bootstrap/';
+// import Weather from './Weather';
 import './App.css'
 
 
@@ -15,8 +15,12 @@ class App extends React.Component {
       showMapAndCityInfo: false,
       renderError: false,
       errorMessage: '',
+      weatherData: [],
+      showWeatherData: false,
+      errorMessageTwo: '',
+      renderErrorTwo: false,
     }
-  } 
+  }
 
   handleSubmit = e => {
     this.setState({
@@ -32,26 +36,84 @@ class App extends React.Component {
     try {
 
       let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_ACCESS_TOKEN}&q=${this.state.searchQuery}&format=json`
-      
+
       let cityResults = await axios.get(url)
-      console.log(cityResults.data)
+      // console.log(cityResults.data)
       this.setState({
         cityData: cityResults.data[0],
         showMapAndCityInfo: true
       })
-      } catch (error){
-          console.log(`Error Occured: ${error.response.status}, City Not Found`);
-        this.setState({
-          renderError: true,
-          errorMessage: `Error Occured: ${error.response.status}, City Not Found`
-        })
-    } 
+
+      // let serverUrl = `${process.env.REACT_APP_SERVER_URL}/weather?searchQuery=${this.state.searchQuery}`;
+      // let weatherResults = await axios.get(serverUrl);
+
+      // console.log(serverUrl);
+      // console.log(weatherResults.data);
+      // this.setState({
+      //   weatherData: weatherResults.data,
+      //   showWeatherData: true,
+      // })
+
+
+    } catch (error) {
+      console.log(`Error Occurred: ${error.response.status}, City Not Found`);
+      this.setState({
+        renderError: true,
+        errorMessage: `Error Occurred: ${error.response.status}, City Not Found`
+      })
+    }
+    this.getWeatherInfo();
+  }
+
+
+  getWeatherInfo = async () => {
+
+    try {
+      let serverUrl = `${process.env.REACT_APP_SERVER_URL}/weather?lat=${this.state.cityData.lat}&lon=${this.state.cityData.lon}`;
+      let weatherResults = await axios.get(serverUrl);
+
+      console.log(serverUrl);
+      console.log(weatherResults.data);
+      this.setState({
+        weatherData: weatherResults.data,
+        showWeatherData: true,
+      })
+
+
+    } catch (error) {
+      console.log(`Error Occurred: ${error.response.status}, City Not Found`);
+      this.setState({
+        renderErrorTwo: true,
+        errorMessageTwo: `Error Occurred: error status 500`
+      })
+    }
+
   }
 
 
 
+
+
+
+
+
+
+
   render() {
-    console.log(this.state.searchQuery);
+    // console.log(this.state);
+    // console.log(this.state.weatherData.data);
+
+    let weatherToRender = this.state.weatherData.map((weather, idx) => (
+      <div>
+
+      <ListGroup.Item key={idx}>
+        Date: {weather.date},
+      </ListGroup.Item>   
+      <ListGroup.Item>
+        High: {weather.max_temp}, Low: {weather.min_temp},    {weather.description}
+      </ListGroup.Item>
+    </div>
+    ));
     return (
       <>
         <header>
@@ -66,19 +128,35 @@ class App extends React.Component {
           </form>
           <Card>
             <div className='error'>
-            <Card.Text>{this.state.renderError}{this.state.errorMessage}</Card.Text>
+              <Card.Text>{this.state.renderError}{this.state.errorMessage}</Card.Text>
             </div>
-          {
-            this.state.showMapAndCityInfo &&
-            <article>
-              
-              <Card.Header><h3>{this.state.cityData.display_name}</h3></Card.Header>
-              <Card.Img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_ACCESS_TOKEN}&zoom=13&center=${this.state.cityData.lat},${this.state.cityData.lon}`} alt={`map of ${this.state.cityData.display_name}`} />
-              <Card.Text>lat: {this.state.cityData.lat} lon: {this.state.cityData.lon}</Card.Text>
-            
-            </article>
-          }
+            <div className='error'>
+              <Card.Text>{this.state.renderErrorTwo}{this.state.errorMessageTwo}</Card.Text>
+            </div>
+            {
+              this.state.showMapAndCityInfo &&
+              <article className='art1'>
+
+                <Card.Header><h3>{this.state.cityData.display_name}</h3></Card.Header>
+                <Card.Img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_ACCESS_TOKEN}&zoom=13&center=${this.state.cityData.lat},${this.state.cityData.lon}`} alt={`map of ${this.state.cityData.display_name}`} />
+                <Card.Text>lat: {this.state.cityData.lat} lon: {this.state.cityData.lon}</Card.Text>
+
+              </article>
+            }
+
+
+
           </Card>
+            {
+              this.state.showWeatherData &&
+              <article>
+               <ListGroup>
+                  <ListGroup.Item>
+                    {weatherToRender}
+                  </ListGroup.Item>
+                </ListGroup>
+              </article>
+            }    
         </main>
       </>
     )
